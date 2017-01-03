3fc97a73d8069e45d545154bedde79996a46d05b
theblackbolt <roberto.hernandez@clearlink.com>
2017-01-03 15:40:59 -0700

    fixed class-style with handlebars curly braces. Now using 2 spaces.

--- dist/htmllint-loader.js
+++ dist/htmllint-loader.js
@@ -39,215 +39,216 @@ var _config = require('./config');
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var isFile = function isFile(file) {
-    try {
-        return _fsExtra2.default.lstatSync(file).isFile();
-    } catch (e) {
-        return false;
-    }
+  try {
+    return _fsExtra2.default.lstatSync(file).isFile();
+  } catch (e) {
+    return false;
+  }
 };
 
 var cleanContent = function cleanContent(content) {
-    var lines = content.split('\n');
-    var pattern = '';
-    var replace = '';
+  var lines = content.split('\n');
+  var pattern = '';
+  var replace = '';
 
-    _lodash2.default.forEach(lines, function (line, i) {
-        lines[i] = lines[i].replace('<?php', '    ');
-        lines[i] = lines[i].replace('<?=', '   ');
-        lines[i] = lines[i].replace('<?', '  ');
-        lines[i] = lines[i].replace('?>', '  ');
+  _lodash2.default.forEach(lines, function (line, i) {
+    lines[i] = lines[i].replace('<?php', '    ');
+    lines[i] = lines[i].replace('<?=', '   ');
+    lines[i] = lines[i].replace('<?', '  ');
+    lines[i] = lines[i].replace('?>', '  ');
+    lines[i] = lines[i].replace(/class="\{\{.*}}"/, 'class="temp"');
 
-        _lodash2.default.forEach(_htmlTags2.default, function (tag) {
-            if (tag.toLowerCase() !== 'title') {
-                pattern = new RegExp('(<' + tag + '>).*(</' + tag + '>)');
-                replace = '<' + tag + '></' + tag + '>';
+    _lodash2.default.forEach(_htmlTags2.default, function (tag) {
+      if (tag.toLowerCase() !== 'title') {
+        pattern = new RegExp('(<' + tag + '>).*(</' + tag + '>)');
+        replace = '<' + tag + '></' + tag + '>';
 
-                lines[i] = lines[i].replace(pattern, replace);
-            }
-        });
+        lines[i] = lines[i].replace(pattern, replace);
+      }
     });
+  });
 
-    return lines.join('\n');
+  return lines.join('\n');
 };
 
 var pluralize = function pluralize(_word, count) {
-    var word = _word;
+  var word = _word;
 
-    if (count > 1) {
-        word += 's';
-    }
+  if (count > 1) {
+    word += 's';
+  }
 
-    return word;
+  return word;
 };
 
 var stylish = function stylish(results) {
-    var errors = 0;
-    var warnings = 0;
-    var output = '';
-    var total = 0;
-    var summaryColor = 'yellow';
-    var messages = null;
-    var filename = null;
-    var tableOptions = null;
-    var fileOutput = null;
-    var styledOutput = null;
-    var tableLayout = null;
-    var format = null;
+  var errors = 0;
+  var warnings = 0;
+  var output = '';
+  var total = 0;
+  var summaryColor = 'yellow';
+  var messages = null;
+  var filename = null;
+  var tableOptions = null;
+  var fileOutput = null;
+  var styledOutput = null;
+  var tableLayout = null;
+  var format = null;
 
-    results.forEach(function (file) {
-        messages = file.messages;
-        filename = _chalk2.default.underline(file.filePath);
-        tableOptions = {
-            align: ['', '  ', 'r', 'l'],
-            stringLength: function stringLength(str) {
-                return _chalk2.default.stripColor(str).length;
-            }
-        };
+  results.forEach(function (file) {
+    messages = file.messages;
+    filename = _chalk2.default.underline(file.filePath);
+    tableOptions = {
+      align: ['', '  ', 'r', 'l'],
+      stringLength: function stringLength(str) {
+        return _chalk2.default.stripColor(str).length;
+      }
+    };
 
-        fileOutput = '\n';
+    fileOutput = '\n';
 
-        if (messages.length === 0) {
-            return;
-        }
-
-        total += messages.length;
-        styledOutput = messages.map(function (message) {
-            var messageType = 'unknown';
-
-            if (message.severity === 'error') {
-                messageType = _chalk2.default.red('error');
-                errors = errors + 1;
-            } else {
-                messageType = _chalk2.default.yellow('warning');
-                warnings = warnings + 1;
-            }
-
-            return ['', _chalk2.default.dim(message.line + ':' + message.column), messageType, _chalk2.default.dim(message.linter || ''), message.reason.replace(/\.$/, '')];
-        });
-        tableLayout = (0, _textTable2.default)(styledOutput, tableOptions);
-        format = tableLayout.split('\n');
-        format = format.join('\n') + '\n\n';
-        fileOutput += filename + '\n';
-        fileOutput += format;
-        output += fileOutput;
-    });
-
-    if (total > 0) {
-        var _bold = ['\u2716 ', total, pluralize(' problem', total), ' (', errors, pluralize(' error', errors), ', ', warnings, pluralize(' warning', warnings), ')\n'];
-
-        if (errors > 0) {
-            summaryColor = 'red';
-        }
-
-        output += _chalk2.default[summaryColor].bold(_bold.join(''));
+    if (messages.length === 0) {
+      return;
     }
 
-    return {
-        message: total > 0 ? output : '',
-        warnings: warnings,
-        errors: errors
-    };
+    total += messages.length;
+    styledOutput = messages.map(function (message) {
+      var messageType = 'unknown';
+
+      if (message.severity === 'error') {
+        messageType = _chalk2.default.red('error');
+        errors = errors + 1;
+      } else {
+        messageType = _chalk2.default.yellow('warning');
+        warnings = warnings + 1;
+      }
+
+      return ['', _chalk2.default.dim(message.line + ':' + message.column), messageType, _chalk2.default.dim(message.linter || ''), message.reason.replace(/\.$/, '')];
+    });
+    tableLayout = (0, _textTable2.default)(styledOutput, tableOptions);
+    format = tableLayout.split('\n');
+    format = format.join('\n') + '\n\n';
+    fileOutput += filename + '\n';
+    fileOutput += format;
+    output += fileOutput;
+  });
+
+  if (total > 0) {
+    var _bold = ['\u2716 ', total, pluralize(' problem', total), ' (', errors, pluralize(' error', errors), ', ', warnings, pluralize(' warning', warnings), ')\n'];
+
+    if (errors > 0) {
+      summaryColor = 'red';
+    }
+
+    output += _chalk2.default[summaryColor].bold(_bold.join(''));
+  }
+
+  return {
+    message: total > 0 ? output : '',
+    warnings: warnings,
+    errors: errors
+  };
 };
 
 var lint = function lint(source, options, webpack) {
-    var messages = [];
-    var lintOptions = _config.defaultOptions;
-    var done = false;
-    var content = _fsExtra2.default.readFileSync(options.resourcePath, 'utf8');
+  var messages = [];
+  var lintOptions = _config.defaultOptions;
+  var done = false;
+  var content = _fsExtra2.default.readFileSync(options.resourcePath, 'utf8');
 
-    if (isFile(options.config)) {
-        lintOptions = (0, _objectAssign2.default)(lintOptions, _fsExtra2.default.readJsonSync(options.config));
-    }
+  if (isFile(options.config)) {
+    lintOptions = (0, _objectAssign2.default)(lintOptions, _fsExtra2.default.readJsonSync(options.config));
+  }
 
-    content = cleanContent(content);
+  content = cleanContent(content);
 
-    (0, _htmllint2.default)(content, lintOptions).then(function (issues) {
-        var _iteratorNormalCompletion = true;
-        var _didIteratorError = false;
-        var _iteratorError = undefined;
+  (0, _htmllint2.default)(content, lintOptions).then(function (issues) {
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
 
-        try {
-            for (var _iterator = issues[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-                var issue = _step.value;
+    try {
+      for (var _iterator = issues[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var issue = _step.value;
 
-                messages.push({
-                    line: issue.line,
-                    column: issue.column,
-                    length: 0,
-                    severity: _config.severity[issue.rule],
-                    reason: _htmllint2.default.messages.renderIssue(issue),
-                    linter: issue.rule
-                });
-            }
-        } catch (err) {
-            _didIteratorError = true;
-            _iteratorError = err;
-        } finally {
-            try {
-                if (!_iteratorNormalCompletion && _iterator.return) {
-                    _iterator.return();
-                }
-            } finally {
-                if (_didIteratorError) {
-                    throw _iteratorError;
-                }
-            }
-        }
-
-        done = true;
-    }, function (err) {
         messages.push({
-            line: 0,
-            column: 0,
-            length: 0,
-            severity: 'warning',
-            reason: err.message,
-            linter: 'unknown'
+          line: issue.line,
+          column: issue.column,
+          length: 0,
+          severity: _config.severity[issue.rule],
+          reason: _htmllint2.default.messages.renderIssue(issue),
+          linter: issue.rule
         });
-
-        done = true;
-    });
-
-    _deasync2.default.loopWhile(function () {
-        return !done;
-    });
-
-    if (messages.length > 0) {
-        var formatted = stylish([{
-            filePath: options.resourcePath,
-            messages: messages
-        }]);
-
-        if (formatted.errors > 0) {
-            if (options.failOnError) {
-                throw new Error('Module failed because of a htmllint error.\n ' + formatted.message);
-            } else {
-                webpack.emitError(formatted.message);
-            }
-        } else if (formatted.warnings > 0) {
-            if (options.failOnWarning) {
-                throw new Error('Module failed because of a htmllint warning.\n ' + formatted.message);
-            } else {
-                webpack.emitWarning(formatted.message);
-            }
+      }
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+          _iterator.return();
         }
+      } finally {
+        if (_didIteratorError) {
+          throw _iteratorError;
+        }
+      }
     }
+
+    done = true;
+  }, function (err) {
+    messages.push({
+      line: 0,
+      column: 0,
+      length: 0,
+      severity: 'warning',
+      reason: err.message,
+      linter: 'unknown'
+    });
+
+    done = true;
+  });
+
+  _deasync2.default.loopWhile(function () {
+    return !done;
+  });
+
+  if (messages.length > 0) {
+    var formatted = stylish([{
+      filePath: options.resourcePath,
+      messages: messages
+    }]);
+
+    if (formatted.errors > 0) {
+      if (options.failOnError) {
+        throw new Error('Module failed because of a htmllint error.\n ' + formatted.message);
+      } else {
+        webpack.emitError(formatted.message);
+      }
+    } else if (formatted.warnings > 0) {
+      if (options.failOnWarning) {
+        throw new Error('Module failed because of a htmllint warning.\n ' + formatted.message);
+      } else {
+        webpack.emitWarning(formatted.message);
+      }
+    }
+  }
 };
 
 module.exports = function htmlLint(source) {
-    var options = (0, _objectAssign2.default)({
-        config: '.htmllintrc',
-        failOnError: true,
-        failWarning: false
-    }, (0, _loaderUtils.parseQuery)(this.query));
+  var options = (0, _objectAssign2.default)({
+    config: '.htmllintrc',
+    failOnError: true,
+    failWarning: false
+  }, (0, _loaderUtils.parseQuery)(this.query));
 
-    options._cwd = process.cwd();
+  options._cwd = process.cwd();
 
-    if (this.resourcePath.indexOf(options._cwd) === 0) {
-        options.resourcePath = this.resourcePath.substr(options._cwd.length + 1);
-    }
+  if (this.resourcePath.indexOf(options._cwd) === 0) {
+    options.resourcePath = this.resourcePath.substr(options._cwd.length + 1);
+  }
 
-    lint(source, options, this);
+  lint(source, options, this);
 
-    return source;
+  return source;
 };
